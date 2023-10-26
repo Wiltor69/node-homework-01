f56be96ff88b8faeece714d4e7033835abacac24
Viacheslav <19wiltor69@gmail.com>
2023-10-26 15:43:43 +0300

    Fourth step

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 const { program } = require("commander");
-const Contacts = require("constants");
+const Contacts = require("./contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await Contacts.listContacts();
+      return console.log(contacts);
+
+    case "get":
+      const contact = await Contacts.getContactById(id);
+      return console.log(contact);
+
+    case "add":
+      const addContact = await Contacts.addContact({ name, email, phone });
+      return console.log(addContact);
+
+    case "remove":
+      const removeContact = await Contacts.removeContact(id);
+      return console.log(removeContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
